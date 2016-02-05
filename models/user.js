fc2199443fda85dcb9db9115388098179870701d
Denis Nefedov <nefedov_de@soshace.com>
2016-02-05 18:26:34 +0300

    Add User model to ORM

---
+++ models/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('user', UserSchema);
