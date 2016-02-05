24df5b4ff4aba9bc1121dbcb3becb47c0ddeb4b7
Denis Nefedov <nefedov_de@soshace.com>
2016-02-05 18:59:09 +0300

    Upgrade posts contollers for auth users

--- public/app/posts/list.controller.js
+++ public/app/posts/list.controller.js
@@ -1,28 +1,57 @@
 (function () {
     'use strict';
 
-    angular
-        .module('posts.controllers')
-        .controller('PostsController', PostsController);
+    angular.module('posts.controllers').controller('PostsController', [
+        '$scope', '$http', '$location', 'Posts', 'User', function ($scope, $http, $location, Posts, User) {
+            $scope.posts = [];
+            $scope.user = {};
+            $scope.user.email = "";
 
-    PostsController.$inject = ['$scope', '$routeParams', 'Posts'];
+            $scope.logout = function() {
+                $http
+                    .post('/auth/logout/')
+                    .then(logoutSuccessCallback, logoutFailCallback);
+            };
 
-    function PostsController($scope, $routeParams, Posts) {
-        $scope.posts = [];
+            getPosts();
+            getEmail();
 
-        activate();
+            function getPosts() {
+                Posts
+                    .all()
+                    .then(getPostsSuccessCallback, getPostsFailCallback);
+            }
 
-        function activate() {
-            Posts.all().then(getSuccessFn, getErrorFn);
+            function getEmail() {
+                User
+                    .getEmail()
+                    .then(getEmailSuccessCallback, getEmailFailCallback);
+            }
+
+            function getPostsSuccessCallback(res, status, headers, config) {
+                $scope.posts = res.data;
+            }
+
+            function getPostsFailCallback(data, status, headers, config) {
+                console.error('Getting posts failed');
+            }
+
+            function getEmailSuccessCallback(res, status, headers, config) {
+                $scope.user.email = res.data;
+            }
+
+            function getEmailFailCallback(data, status, headers, config) {
+                console.error('Getting email failed');
+            }
+
+            function logoutSuccessCallback(res, status, headers, config) {
+                $location.path('/auth/');
+            }
+
+            function logoutFailCallback(data, status, headers, config) {
+                console.error('Logging out failed');
+            }
         }
+    ]);
 
-        function getSuccessFn(res, status, headers, config) {
-            $scope.posts = res.data;
-        }
-
-        function getErrorFn(data, status, headers, config) {
-            console.error('error loading detail');
-        }
-
-    }
 })();
