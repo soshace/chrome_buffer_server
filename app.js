cf17185d28dee6703eb8fd0cd4a14b451ce18066
Denis Nefedov <nefedov_de@soshace.com>
2016-02-05 18:51:49 +0300

    Set express app through Bootable

--- app.js
+++ app.js
@@ -1,82 +1,43 @@
+'use strict';
+
 var express = require('express'),
     https = require('https'),
     http = require('http'),
-    bodyParser = require('body-parser'),
-    morgan = require('morgan'),
+    //morgan = require('morgan'),
     app = express(),
     fs = require('fs'),
-    _ = require('underscore'),
-    fakeDB
+
+    config = require('nconf'),
+    bootable = require('bootable'),
+    bootableEnv = require('bootable-environment')
     ;
 
 var options = {
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
 };
-app.use(morgan('combined'));
 
-app.use("/", express.static(__dirname + '/public'));
+app = bootable(app);
 
-app.get('/posts', function (req, res) {
-    fakeDB = require('./posts.json');
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-    res.end(JSON.stringify(fakeDB, null, 4));
-});
+//app.use(morgan('combined'));
 
-app.get('/posts/:id', function (req, res) {
-    var user,
-        id = parseInt(req.params.id)
-        ;
-    fakeDB = require('./posts.json');
+// Setup initializers
+app.phase(bootable.initializers('setup/initializers/'));
 
-    user = _.findWhere(fakeDB, {id: id});
-    if (user) {
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-        res.end(JSON.stringify(user, null, 4));
-    } else {
-        res.status(404).send('Not found');
-    }
-});
+// Setup environments
+app.phase(bootableEnv('setup/environments/', app));
 
-app.put('/posts', [
-    bodyParser.json(),
-    function (req, res) {
-        var last, newPost;
+// Boot app
+app.boot(function(err) {
+    if (err) throw err;
 
-        fakeDB = require('./posts.json');
-        last = _.max(fakeDB, function (obj) {
-            return obj.id
-        });
-        newPost = req.body;
-        newPost.id = last.id + 1;
-        fakeDB.push(newPost);
+    var httpServer = http.createServer(app);
+    var httpsServer = https.createServer(options, app);
 
-        console.log(newPost);
-        fs.writeFile('./posts.json', JSON.stringify(fakeDB, null, 4), function (err) {
-            if (err) throw err;
-            console.log('It\'s saved!');
-            res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-            res.end(JSON.stringify(fakeDB, null, 4));
-        });
-    }
-]);
-
-app.delete('/posts/:id', function (req, res) {
-    var id = parseInt(req.params.id);
-    fakeDB = require('./posts.json');
-
-    fakeDB = _.reject(fakeDB, {id: id});
-
-    fs.writeFile('./posts.json', JSON.stringify(fakeDB, null, 4), function (err) {
-        if (err) throw err;
-        console.log('It\'s saved!');
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-        res.end(JSON.stringify(fakeDB, null, 4));
+    httpServer.listen(8080, function() {
+        console.log('Express http listen port on 8080');
+    });
+    httpsServer.listen(8081, function() {
+        console.log('Express https listen port on 8081');
     });
 });
-
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(options, app);
-
-httpServer.listen(8080);
-httpsServer.listen(8081);
