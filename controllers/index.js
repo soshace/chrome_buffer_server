b20d51739c6ba235b8d8de5b852aedb6d5283705
Denis Nefedov <nefedov_de@soshace.com>
2016-02-05 18:48:53 +0300

    Add express common router

---
+++ controllers/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express'),
+    router = express.Router();
+
+router.get('/', function(req, res) {
+    if (!req.user) {
+        return res.redirect('/auth');
+    } else {
+        return res.redirect('/posts');
+    }
+});
+
+router.use('/auth', require('./users'));
+router.use('/posts', require('./posts'));
+
+router.use('/api', require('./api/posts'));
+router.use('/api', require('./api/users'));
+
+module.exports = router;
