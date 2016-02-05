d033105f98b2aa02de9fdadfadfc2168a6120c42
Denis Nefedov <nefedov_de@soshace.com>
2016-02-05 18:57:01 +0300

    Remove old posts directives

--- public/app/posts.js
+++ public/app/posts.js
@@ -2,15 +2,11 @@
     'use strict';
 
     angular.module('posts', [
-            'posts.routes',
-            'posts.controllers',
-            'posts.directives',
-            'posts.services'
-        ]);
+        'posts.controllers',
+        'posts.services'
+    ]);
 
     angular.module('posts.controllers', []);
-    angular.module('posts.directives', []);
     angular.module('posts.services', []);
-    angular.module('posts.routes', ['ngRoute']);
 
 })();
