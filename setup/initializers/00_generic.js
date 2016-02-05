cdfe564284b990c63ee0cb2f0eed1283752590ae
Denis Nefedov <nefedov_de@soshace.com>
2016-02-05 18:30:17 +0300

    Introduce nconf

---
+++ setup/initializers/00_generic.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var config    = require('nconf');
+var pwd       = process.env.PWD;
+
+// end of dependencies.
+
+
+module.exports = function() {
+    // Setup nconf
+    /*
+    config.file({ 'file': pwd + '/config.json' });
+    */
+};
